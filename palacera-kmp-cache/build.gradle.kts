70422ae0d0f9cb8a5dff42b97ab14ccb7c1e56f3
aiithak <andrewskeller@gmaill.com>
2024-03-04 21:00:16 -0300

    Jitpack setup

--- palacera-kmp-cache/build.gradle.kts
+++ palacera-kmp-cache/build.gradle.kts
@@ -17,7 +17,9 @@ kotlin {
 //        browser()
 //    }
 
-    androidTarget()
+    androidTarget {
+        publishLibraryVariants("release")
+    }
 
 //    iosX64()
 //    iosArm64()
@@ -72,7 +74,7 @@ publishing {
             // Adjust groupId, artifactId, and version according to your needs
             group = "com.palacera.kmpcache"
             artifactId = "kmpcache"
-            version = "0.0.5"
+            version = "0.0.6"
 
             // Include artifacts from Kotlin Multiplatform targets
             from(components["kotlin"])
