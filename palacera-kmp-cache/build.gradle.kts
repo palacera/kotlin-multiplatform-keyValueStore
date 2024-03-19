e4fa76be9074d205d702ff2a53485f832d827048
aiithak <andrewskeller@gmaill.com>
2024-03-19 18:10:37 -0300

    Update publish version

--- palacera-kmp-cache/build.gradle.kts
+++ palacera-kmp-cache/build.gradle.kts
@@ -74,7 +74,7 @@ publishing {
             // Adjust groupId, artifactId, and version according to your needs
             group = "com.palacera.kmpcache"
             artifactId = "kmpcache"
-            version = "0.0.8"
+            version = "0.0.9"
 
             // Include artifacts from Kotlin Multiplatform targets
             from(components["kotlin"])
